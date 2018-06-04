473532b921933cf77acfaa42cc8feea6f1552be7
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-06-04 00:52:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+		 
 		programmer_hash[:dennis_ritchie][:languages][0]
 end
 
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
-		programmer_hash[]
+		programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
